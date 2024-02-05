1799c86bdfc9e8b2c2735e9ca9a56c54114fc1c4
paranoidclark <clarkkemparan@gmail.com>
2024-02-05 15:12:34 +0800

    RPS with UI. Can't stop the score from adding after 5.

--- script.js
+++ script.js
@@ -13,38 +13,63 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    const playerChose = document.querySelector('.playerChose');
+    const computerChose = document.querySelector('.computerChose');
+    const results = document.querySelector('.results');
+
+    playerChose.textContent = `You chose: ${playerSelection}`
+    computerChose.textContent = `Computer chose: ${computerSelection}`
 
     if (playerSelection == computerSelection) {
-        console.log("It's a tie!");
+        results.textContent = "It's a tie!"
     } else if (
         (playerSelection == "Rock" && computerSelection == "Scissors") ||
         (playerSelection == "Scissors" && computerSelection == "Paper") ||
         (playerSelection == "Paper" && computerSelection == "Rock")
     ) {
-        console.log("You Won!")
+        results.textContent = "You Won!"
+        playerScore++;
+        playerShow.textContent = `Player Score: ${playerScore}`;
+        checkWinner();
     } else {
-        console.log("You Lose!");
+        results.textContent = "You Lose!"
+        computerScore++;
+        computerShow.textContent = `Computer Score: ${computerScore}`;
+        checkWinner();
     }
 }
 
-function playGame() {
-
-    let rounds = 0;
-
-    while (rounds < 5) {
-        console.log(`The Computer chose ${computerSelection}`);
-        console.log(`You chose ${playerSelection}`);
-        console.log((playRound(playerSelection, computerSelection)));
-        rounds++;
-    }
-}
+const btn = document.querySelectorAll('.btn');
+const h1 = document.querySelector('h1')
+const container = document.querySelector('.container')
+const playerShow = document.querySelector('.playerScore')
+const computerShow = document.querySelector('.computerScore')
 
 let playerScore = 0;
 let computerScore = 0;
 
-const playerSelection = prompt("Rock, Paper, Scissors?");
-const computerSelection = getComputerChoice();
+function checkWinner() {
 
-playGame();
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            h1.textContent = "You Won against the Computer! Congratulations!"
+        } else {
+            h1.textContent = "You lost against the Computer. Better luck next time."
+        }
+    }
+}
+
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => playRound(btn[i].value, getComputerChoice()))
+}
+
+
+
+
+
+
+
+
 
 
